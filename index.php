e568c9d432dddb8bdc9f25128c99e406ab6abbf9
MarcoAgosta <agostamarco@libero.it>
2022-12-20 15:54:37 +0100

    Dati stampati in maniera rozza

--- index.php
+++ index.php
@@ -40,4 +40,49 @@
 
     ];
 
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+</head>
+<body>
+
+    <table>
+        <thead>
+            <th>Nome</th>
+            <th>Descrizione</th>
+            <th>Parcheggio</th>
+            <th>Voto</th>
+            <th>Distanza dal centro</th>
+        </thead>
+
+        <tbody>
+            <?php
+
+                foreach ($hotels as $hotel){
+                    echo "<tr>";
+                    echo "<td>{$hotel['name']}</td>";
+                    echo "<td>{$hotel['description']}</td>";
+                    if ($hotel['parking']==true){
+                        echo "<td>Presente</td>";
+                    } else {
+                        echo "<td>Assente</td>";
+                    };
+                    echo "<td>{$hotel['vote']}</td>";
+                    echo "<td>{$hotel['distance_to_center']} Km</td>";
+                    echo "</tr>";
+                };
+
+            ?>
+        </tbody>
+
+    </table>
+    
+</body>
+</html>
